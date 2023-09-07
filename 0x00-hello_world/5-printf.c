4aba29987e454bad553f7de26879d865bc8213af
StevOti <steveotieno5640@gmail.com>
2023-09-07 08:58:43 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcom is a peice of art,\n");
+return (0);
+}
